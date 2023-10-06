8df86f79728c97bf151994a15ed7038539d2e916
Mariano <97608505+Ferbinand@users.noreply.github.com>
2023-10-06 00:14:41 -0300

    1coimmit

---
+++ motor.js
@@ -0,0 +1,24 @@
+let btn = document.getElementById('sobremiBtn');
+let sobremiSection = document.getElementById('sobremi');
+
+btn.addEventListener('click', () => {
+    sobremiSection.scrollIntoView({ behavior: 'smooth' });
+  });
+
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      window.scrollTo({
+          top: targetSection.offsetTop,
+          behavior: 'smooth'
+      });
+  });
+});
+
